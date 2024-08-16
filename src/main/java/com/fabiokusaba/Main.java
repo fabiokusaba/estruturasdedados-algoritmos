63cde3a3471048c9dc0d932f6837c5c02ee39af5
fabiokusaba <fabinhokb44@gmail.com>
2024-08-15 21:35:25 -0300

    Introdução a estruturas de dados e algoritmos

---
+++ src/main/java/com/fabiokusaba/Main.java
@@ -0,0 +1,9 @@
+package com.fabiokusaba;
+
+// O que são estruturas de dados? É uma estrutura que a gente utiliza para armazenar e organizar dados ou informações
+// de modo que esses dados possam ser facilmente manipulados e acessados da forma mais eficiente possível
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}

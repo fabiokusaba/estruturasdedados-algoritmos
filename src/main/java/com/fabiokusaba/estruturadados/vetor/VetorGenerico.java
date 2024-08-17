0c290f9b4674ffff87958d5eac3138af9a29d9a8
fabiokusaba <fabinhokb44@gmail.com>
2024-08-17 18:26:05 -0300

    Generalizar o tipo do vetor

---
+++ src/main/java/com/fabiokusaba/estruturadados/vetor/VetorGenerico.java
@@ -0,0 +1,104 @@
+package com.fabiokusaba.estruturadados.vetor;
+
+// Generalizar a classe para que ela possa trabalhar com qualquer tipo do Java seja um inteiro, double ou uma classe
+// Aprendemos que a super classe ou a classe mãe de todas as classes no Java é a classe 'Object', então a maneira mais
+// simples de deixar a nossa classe genérica seria trocar o 'String' por 'Object'
+public class VetorGenerico {
+    private Object[] elementos;
+    private int tamanho;
+
+    public VetorGenerico(int capacidade) {
+        this.elementos = new Object[capacidade];
+        this.tamanho = 0;
+    }
+
+    public boolean adiciona(Object elemento) {
+        this.aumentaCapacidade();
+
+        if (this.tamanho < this.elementos.length) {
+            this.elementos[this.tamanho] = elemento;
+            this.tamanho++;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean adiciona(Object elemento, int posicao) {
+        if (!(posicao >= 0 && posicao < this.tamanho)) {
+            throw new IllegalArgumentException("Posição inválida");
+        }
+        this.aumentaCapacidade();
+
+        for (int i = this.tamanho - 1; i >= posicao; i--) {
+            this.elementos[i + 1] = this.elementos[i];
+        }
+
+        this.elementos[posicao] = elemento;
+        this.tamanho++;
+        return true;
+    }
+
+    private void aumentaCapacidade() {
+        if (this.tamanho == this.elementos.length) {
+            Object[] elementosNovos = new Object[this.elementos.length * 2];
+
+            for (int i = 0; i < this.elementos.length; i++) {
+                elementosNovos[i] = this.elementos[i];
+            }
+
+            this.elementos = elementosNovos;
+        }
+    }
+
+    public Object busca(int posicao) {
+        if (!(posicao >= 0 && posicao < this.tamanho)) {
+            throw new IllegalArgumentException("Posição inválida");
+        }
+
+        return this.elementos[posicao];
+    }
+
+    public int busca(Object elemento) {
+        for (int i = 0; i < this.tamanho; i++) {
+            if (this.elementos[i].equals(elemento)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public void  remove(int posicao) {
+        if (!(posicao >= 0 && posicao < this.tamanho)) {
+            throw new IllegalArgumentException("Posição inválida");
+        }
+
+        for (int i = posicao; i < this.tamanho - 1; i++) {
+            this.elementos[i] = this.elementos[i + 1];
+        }
+
+        this.tamanho--;
+    }
+
+    // Metodo responsável por retornar o tamanho do nosso vetor
+    public int tamanho() {
+        return this.tamanho;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+
+        for (int i = 0; i < this.tamanho - 1; i++) {
+            sb.append(this.elementos[i]);
+            sb.append(", ");
+        }
+
+        if (this.tamanho > 0) {
+            sb.append(this.elementos[this.tamanho - 1]);
+        }
+
+        sb.append("]");
+        return sb.toString();
+    }
+}
---
+++ src/main/java/com/fabiokusaba/estruturadados/vetor/teste/Aula10.java
@@ -0,0 +1,36 @@
+package com.fabiokusaba.estruturadados.vetor.teste;
+
+import com.fabiokusaba.estruturadados.vetor.VetorGenerico;
+
+public class Aula10 {
+    public static void main(String[] args) {
+        VetorGenerico vetor = new VetorGenerico(3);
+
+        Contato c1 = new Contato("Contato 1", "1234-4567", "contato1@email.com");
+        Contato c2 = new Contato("Contato 2", "2345-6789", "contato2@email.com");
+        Contato c3 = new Contato("Contato 3", "3456-7890", "contato3@email.com");
+
+        Contato c4 = new Contato("Contato 3", "3456-7890", "contato3@email.com");
+
+        vetor.adiciona(c1);
+        vetor.adiciona(c2);
+        vetor.adiciona(c3);
+
+        // Isso aqui é um auto boxing, esse processo de você passar um tipo primitivo e ele ser transformado em um
+        // objeto se chama auto boxing no Java
+//        vetor.adiciona(3);
+//        vetor.adiciona(4);
+//        vetor.adiciona(5);
+
+        System.out.println("Tamanho = " + vetor.tamanho());
+
+        int pos = vetor.busca(c4);
+        if (pos > -1) {
+            System.out.println("Elemento existe no vetor");
+        } else {
+            System.out.println("Elemento não existe no vetor");
+        }
+
+        System.out.println(vetor);
+    }
+}

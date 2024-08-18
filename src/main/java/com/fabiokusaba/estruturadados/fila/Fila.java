819e2cdc455510d156177f39820b8e3150b06803
fabiokusaba <fabinhokb44@gmail.com>
2024-08-18 18:23:11 -0300

    Filas: Enfileirar (adicionar) elemento

--- src/main/java/com/fabiokusaba/estruturadados/fila/Fila.java
+++ src/main/java/com/fabiokusaba/estruturadados/fila/Fila.java
@@ -11,4 +11,24 @@ public class Fila<T> extends EstruturaEstatica<T> {
     public Fila(int capacidade) {
         super(capacidade);
     }
+
+    public void enfileira(T elemento) {
+        // O algoritmo básico seria:
+//        this.elementos[this.tamanho] = elemento;
+//        this.tamanho++;
+
+        // Só que ainda tem o problema da questão da capacidade que vimos nas aulas de vetores, pelo vetor ser uma
+        // estrutura estática ele tem capacidade finita e é por isso que nas aulas de vetores a gente desenvolveu o
+        // metodo 'aumentaCapacidade' que vai verificar o tamanho com a capacidade real do nosso vetor, se atingiu essa
+        // capacidade ele dobra a capacidade e transfere todos os elementos que estão no nosso vetor para esse novo
+        // vetor com mais capacidade
+        // Então, aqui antes de enfileirar a gente poderia chamar o 'aumentaCapacidade' para poder aumentar a capacidade
+        // desse vetor e garantir ou como estamos adicionando o elemento no final nós já temos o metodo aqui na
+        // 'EstruturaEstatica' que cuida disso que é o metodo protegido 'adiciona'
+        // Então, se você quiser reutilizar código a gente pode simplesmente fazer a chamada desse metodo aqui
+        this.adiciona(elemento);
+
+        // Ou caso a gente queira simplificar esse algoritmo aqui
+//        this.elementos[this.tamanho++] = elemento;
+    }
 }

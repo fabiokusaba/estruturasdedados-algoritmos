c9512f1ad3d9b506f2b62a833a8691eff47ef216
fabiokusaba <fabinhokb44@gmail.com>
2024-08-18 09:20:26 -0300

    Pilhas: Introdução

---
+++ src/main/java/com/fabiokusaba/estruturadados/pilha/Pilha.java
@@ -0,0 +1,13 @@
+package com.fabiokusaba.estruturadados.pilha;
+
+import com.fabiokusaba.estruturadados.base.EstruturaEstatica;
+
+public class Pilha<T> extends EstruturaEstatica<T> {
+    public Pilha() {
+        super();
+    }
+
+    public Pilha(int capacidade) {
+        super(capacidade);
+    }
+}
